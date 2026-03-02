5f49ad7e87de36bc3f09957a123f754ccabeef54
Yuuki6152 <nakayk0222@yahoo.co.jp>
2026-03-03 07:06:10 +0900

    Initial commit: ASP.NET Core MVC + React(Vite)

---
+++ CoreFusion/ClientApp/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    root: 'src', // ソースコードのルートをsrcに設定
+    //ビルド成果物の参照先の基準URL
+    base: '/js/react/',
+
+    build: {
+        outDir: path.resolve(__dirname, '../wwwroot/js/react'), // ビルド結果をMVCのwwwroot/js/reactに出力
+        emptyOutDir: true, // 出力ディレクトリをクリーンアップ
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src/main.tsx'), // エントリーポイント
+                //別のReactアプリやコンポーネントがある場合、ここに追加できる
+                //otherApp: path.resolve(__dirname, 'src/OtherApp/main.tsx'),
+            },
+            output: {
+                entryFileNames: '[name].js', // エントリーポイントのファイル名を main.js とする
+                chunkFileNames: 'chunks/[name]-[hash].js', // 分割されたチャンクのファイル名
+                assetFileNames: 'assets/[name]-[hash][extname]', // 静的アセットのファイル名
+            }
+        }
+    }
+});
